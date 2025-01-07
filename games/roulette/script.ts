3c0aaaf95e943bd6a5e648f3aa2e1fc5d67dfc06
sharmnten <sharmnten@gmail.com>
2025-01-07 05:53:16 +0000

    Add game pages and integrate Appwrite client for session management

---
+++ games/roulette/script.ts
@@ -0,0 +1,10 @@
+import { Account, Client } from 'appwrite';
+
+const client = new Client();
+
+client
+    .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
+    .setProject('5fca4f87d48c2') // Your project ID
+
+const account = new Account(client);
+
---
+++ games/script.ts
@@ -0,0 +1,19 @@
+import {Client, Account} from 'appwrite';
+const client = new Client();
+client
+    .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
+    .setProject('5fca4f87d48c2') // Your project ID
+const account = new Account(client);
+document.addEventListener('DOMContentLoaded', () => {
+    const greeting = document.getElementById('greeting');
+    try{
+    let sessionId = localStorage.getItem("session")
+    //const session = account.getSession(sessionId);
+    }catch(error){
+        console.error('Session not found:', error);
+        window.location.href = '/';
+    }
+
+
+
+});
